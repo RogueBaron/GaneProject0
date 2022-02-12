324c412aebfc836e5559cdce676bcf61d25eff51
Daniel Koutris <dangvk3@gmail.com>
2022-02-11 21:16:33 -0600

    Add project files.

---
+++ GaneProject0/Game1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GaneProject0
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Bidoof bidoof;
+        private Phanphy phanphy;
+        private Money money;
+        private Krabs krabs;
+        private MrKrabs mrKrabs;
+        SpriteFont spriteFont;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            bidoof = new Bidoof(this, Color.White) { Position = new Vector2(0, 20) };
+            phanphy = new Phanphy(this, Color.White) { Position = new Vector2(400, 100) };
+            money = new Money(this, Color.White) { Position = new Vector2(100, 300) };
+            krabs = new Krabs(this, Color.White) { Position = new Vector2(200, 20) };
+            mrKrabs = new MrKrabs(this, Color.White) { Position = new Vector2(200, 100) };
+
+            
+
+            base.Initialize();
+            
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            bidoof.LoadContent();
+            phanphy.LoadContent();
+            money.LoadContent();
+            krabs.LoadContent();
+            mrKrabs.LoadContent();
+
+            spriteFont = Content.Load<SpriteFont>("Font");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+
+            bidoof.Draw(_spriteBatch);
+            phanphy.Draw(_spriteBatch);
+            money.Draw(_spriteBatch);
+            krabs.Draw(_spriteBatch);
+            mrKrabs.Draw(_spriteBatch);
+            _spriteBatch.DrawString(spriteFont, "Press Escape To Get Out Of Here!", new Vector2(300, 300), Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GaneProject0/Krabs.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GaneProject0
+{
+    public class Krabs
+    {
+        Random random;
+        /// <summary>
+        /// The game this ball is a part of
+        /// </summary>
+        Game game;
+
+        /// <summary>
+        /// A color to help distinguish one ball from another
+        /// </summary>
+        Color color;
+
+        /// <summary>
+        /// The texture to apply to a ball
+        /// </summary>
+        Texture2D texture;
+
+        /// <summary>
+        /// The position of the ball in the game world
+        /// </summary>
+        public Vector2 Position { get; set; }
+
+        /// <summary>
+        /// Constructs a new ball instance
+        /// </summary>
+        /// <param name="game">The game this ball belongs in</param>
+        /// <param name="color">A color to distinguish this ball</param>
+        public Krabs(Game game, Color color)
+        {
+            this.game = game;
+            this.color = color;
+            this.random = new Random();
+        }
+
+        // Helper for reading input from keyboard, gamepad, and touch input. This class 
+        // tracks both the current and previous state of the input devices, and implements 
+        // query methods for high level input actions such as "move up through the menu"
+        // or "pause the game".
+
+
+        /// <summary>
+        /// Loads the ball's texture
+        /// </summary>
+        public void LoadContent()
+        {
+            texture = game.Content.Load<Texture2D>("krabs");
+        }
+
+        /// <summary>
+        /// Draws the ball at its current position and with 
+        /// its assigned color
+        /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch to render with</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (texture is null) throw new InvalidOperationException("Texture must be loaded to render");
+            spriteBatch.Draw(texture, Position, color);
+        }
+
+    }
+}
